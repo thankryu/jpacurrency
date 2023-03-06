ff8b04fd13e496eb368a4c94e72ec627badd7e66
Kyounghan Ryu <thankryu@gmail.com>
2023-03-06 23:05:04 +0900

    Initial commit

---
+++ src/main/java/jpamoney/jpacurrency/JpacurrencyApplication.java
@@ -0,0 +1,13 @@
+package jpamoney.jpacurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpacurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpamoney/jpacurrency/JpacurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpamoney.jpacurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
